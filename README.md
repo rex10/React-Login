f63e2560c1d8f7b5e07e96e8f202361c78ae03df
Rex <rex.christian1@gmail.com>
2020-10-27 04:28:54 +0530

    React simple login module

---
+++ README.md
@@ -0,0 +1,14 @@
+ 
+## To Run this project flow this steps
+
+# First Step
+- Clone the repository ` https://github.com/rex10/Login-notes-module.git `
+
+# Second Step 
+- Install packages ` npm install` or `yarn install`
+
+# Third Step
+- Run server `npm run server`
+
+# Fourth Step
+- Run Application `npm start`
---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+
+import Login from './Login';
+import Dashboard from './Dashboard';
+import Home from './Home';
+import PrivateRoute from './Utils/PrivateRoute';
+import PublicRoute from './Utils/PublicRoute';
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+            <NavLink activeClassName="active" to="/login">Login</NavLink>
+            <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink>
+          </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <PublicRoute path="/login" component={Login} />
+              <PrivateRoute path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
